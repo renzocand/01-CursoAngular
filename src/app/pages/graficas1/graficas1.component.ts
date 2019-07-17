5dfad8d871fc87ff0ea0ffc34323996438fc6ba9
Renzo <renzocand@gmail.com>
2019-07-17 17:12:45 -0500

    termino inputs y outputs

--- src/app/pages/graficas1/graficas1.component.ts
+++ src/app/pages/graficas1/graficas1.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ChartType } from 'chart.js';
-import { MultiDataSet, Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-graficas1',
